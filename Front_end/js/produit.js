0f0547038faed7379e18ba9ad021d42c67887102
martinfasq <fasquel.martin@gmail.com>
2021-08-26 16:19:50 +0200

    modif fiche produit

--- Front_end/js/produit.js
+++ Front_end/js/produit.js
@@ -1,13 +1,17 @@
-console.log("produit");
-
 function getProductData() {
   fetch("http://localhost:3000/api/teddies/5be9c8541c9d440000665243")
     .then((res) => res.json())
-    //   .then((dataValue) => console.log(dataValue))
     .then((dataValue) => showData(dataValue));
 }
 getProductData();
 
 function showData(data) {
-  document.getElementById("name").textContent = data.name;
+  document.getElementById("nameProduct").textContent = data.name;
+  document.getElementById("priceProduct").textContent = (data.price + " €") ;
+  document.getElementById("imgProduct").setAttribute("src", data.imageUrl);
+  document.getElementById("descriptionProduct").textContent = data.description;
+  document.getElementById("optionSelect").textContent = data.colors;
 }
+
+
+
